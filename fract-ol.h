c0e8c4c0a134835505b73dce2e5ba3150b9def0a
Sanghoon Lee <salee2@c5r2s5.42seoul.kr>
2022-09-15 20:23:16 +0900

    cordinate transform

---
+++ fract-ol.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract-ol.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salee2 <salee2@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 15:59:50 by salee2            #+#    #+#             */
+/*   Updated: 2022/09/14 15:59:54 by salee2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACT_OL_H
+#define FRACT_OL_H
+
+#include <unistd.h>
+#include "minilibx_opengl_20191021/mlx.h"
+
+typedef struct s_complex
+{
+	double	real;
+	double	imag;
+}	t_complex;
+
+#endif //FRACT_OL_H
--- main.c
+++ main.c
@@ -6,11 +6,50 @@
 /*   By: salee2 <salee2@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 19:20:34 by salee2            #+#    #+#             */
-/*   Updated: 2022/09/12 19:20:35 by salee2           ###   ########.fr       */
+/*   Updated: 2022/09/13 19:55:37 by salee2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx_opengl_20191021/mlx.h"
+#include "fract-ol.h"
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+
+int deal_key(int key, void *mlx_ptr, void *win_ptr)
+{
+	t_complex com;
+
+	com.real = 0.0;
+	com.imag = 0.0;
+
+	for(double y0=-3.0; y0<3.0; y0 += 0.01)
+	{
+		for(double x0=-3.0; x0<3.0; x0 += 0.01)
+		{
+			int i = 0;
+			double y = 0;
+			double x = 0;
+			while(y * y + x *x <= 2*2 && ++i <1000 )
+			{
+				double xtemp = x * x - y * y + x0;
+				y = 2 * x * y + y0;
+				x = xtemp;
+			}
+			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x000fff);
+		}
+	}
+	return 0;
+}
+
+//
+//int deal_key(int key, void *param)
+//{
+//	ft_putchar('x');
+//	return 0;
+//}
 
 int	main()
 {
@@ -19,7 +58,12 @@ int	main()
 
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
-	mlx_loop(mlx_ptr);
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xffffff);
+//	mlx_key_hook(win_ptr, deal_key,(void *)0);
+	mlx_key_hook(win_ptr, deal_key( 0, mlx_ptr, win_ptr), (void *)0);
+
+	 mlx_loop(mlx_ptr);
+
 
 	return (0);
 
